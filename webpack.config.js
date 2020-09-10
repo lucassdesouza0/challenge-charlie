bb262c2fa2afd27d6dc994bce0668953337cfa7a
Lucas Santos de Souza <lucas.santos@zoom.com.br>
2020-09-09 21:52:54 -0300

    docker config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,48 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const WebpackMessages = require("webpack-messages");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WebpackMessages = require('webpack-messages');
 
 module.exports = {
-    mode: "none",
+    mode: 'none',
     entry: {
-        app: path.join(__dirname, "src", "index.tsx"),
+        app: path.join(__dirname, 'src', 'index.tsx'),
     },
-    target: "web",
+    target: 'web',
     resolve: {
-        extensions: [".ts", ".tsx", ".js"],
+        extensions: ['.ts', '.tsx', '.js'],
     },
-    devtool: "inline-source-map",
-    stats: "errors-warnings",
+    devtool: 'inline-source-map',
+    stats: 'errors-warnings',
     devServer: {
-        stats: "errors-only",
+        stats: 'errors-only',
+        port: 3000,
+        host: '0.0.0.0',
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: "/node_modules/",
+                use: 'ts-loader',
+                exclude: '/node_modules/',
             },
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                use: ['style-loader', 'css-loader'],
             },
         ],
     },
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "build"),
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, "public", "index.html"),
+            template: path.join(__dirname, 'public', 'index.html'),
         }),
         new WebpackMessages({
-            name: "client",
+            name: 'client',
             logger: (str) => console.log(`>> ${str}`),
         }),
     ],
